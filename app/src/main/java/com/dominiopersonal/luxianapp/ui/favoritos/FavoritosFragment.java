012c2366bf80e2641854c761423e44754b0ed3d7
dpb0001 <dpb0001@alu.medac.es>
2022-05-21 18:25:24 +0200

    Versión Inicial

--- app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.dominiopersonal.luxianapp.databinding.FragmentFavoritosBinding;
-import com.dominiopersonal.luxianapp.ui.favoritos.FavoritosViewModel;
 
 public class FavoritosFragment extends Fragment {
 
