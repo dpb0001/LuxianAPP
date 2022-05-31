e85f9ee1b0cb740e3e0dc5d8f1964ac7bd731134
NombreTemporalParaClase <dsi0001@alu.medac.es>
2022-05-31 20:30:20 +0200

    Crear horizontalLayout(1ªparte)

---
+++ app/src/main/java/com/dominiopersonal/luxianapp/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.dominiopersonal.luxianapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public  RecyclerViewAdapter(ArrayList<String> names, ArrayList<String> imageUrls, Context context){
+        mNames = names;
+        mImageUrls = imageUrls;
+        mContext = context;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onCreateViewHolder: Called");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on a image: " + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            image = itemView.findViewById(R.id.image_view);
+            name = itemView.findViewById(R.id.name);
+
+        }
+    }
+}
