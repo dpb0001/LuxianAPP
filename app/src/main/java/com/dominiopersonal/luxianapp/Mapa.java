b2746f3843f0e6735e1ff9cc2923cfa159bebced
dpb0001 <dpb0001@alu.medac.es>
2022-06-06 17:25:55 +0200

    Funcionan los cambios de Dawid y mios

--- app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
@@ -2,16 +2,14 @@ package com.dominiopersonal.luxianapp;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
-
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
