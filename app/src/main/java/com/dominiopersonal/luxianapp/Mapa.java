7f4e387ae80626ce651ccad9026a9014e84c6cd8
dpb0001 <dpb0001@alu.medac.es>
2022-06-06 16:07:10 +0200

    Pruebas de Mapas. Funciona correctamente

--- app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
@@ -1,4 +1,44 @@
 package com.dominiopersonal.luxianapp;
 
-public class Mapa {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mapa;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapa);
+
+        //Desde Esta parte estamos cargando el mapa, para poder conectarlo con la vista
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mapa = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mapa.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("La marca se encuentra en Sidney"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
--- app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
@@ -1,18 +1,27 @@
 package com.dominiopersonal.luxianapp.ui.favoritos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.dominiopersonal.luxianapp.AccesoApp;
+import com.dominiopersonal.luxianapp.Mapa;
+import com.dominiopersonal.luxianapp.R;
 import com.dominiopersonal.luxianapp.databinding.FragmentFavoritosBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FavoritosFragment extends Fragment {
+
+    Button btnPruebasMapas;
 
     private FragmentFavoritosBinding binding;
 
@@ -24,8 +33,21 @@ public class FavoritosFragment extends Fragment {
         binding = FragmentFavoritosBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textFavoritos;
-        favoritosViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        btnPruebasMapas = root.findViewById(R.id.btnPruebaMapas);
+
+        btnPruebasMapas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), Mapa.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
         return root;
     }
 
