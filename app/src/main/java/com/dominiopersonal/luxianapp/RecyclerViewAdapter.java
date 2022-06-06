cb6613819b752e2a2c954b80a939075aaa6cfcad
NombreTemporalParaClase <dsi0001@alu.medac.es>
2022-06-06 19:21:05 +0200

    Cambios insignificantes

--- app/src/main/java/com/dominiopersonal/luxianapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/RecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.dominiopersonal.luxianapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,7 +40,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Glide.with(mContext)
                 .asBitmap()
                 .load(mImageUrls.get(position))
@@ -49,7 +51,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(mContext,mNames.get(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext,mNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
 
