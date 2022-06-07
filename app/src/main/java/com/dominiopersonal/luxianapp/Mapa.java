9e6513d3f9488192e03fa12aead4908c0da29311
dpb0001 <dpb0001@alu.medac.es>
2022-06-07 12:55:58 +0200

    Se pone un puntero en localización en tiempo real, se ponen dos marcadores cerca de MEDAC

--- app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/Mapa.java
@@ -1,14 +1,24 @@
 package com.dominiopersonal.luxianapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -33,10 +43,64 @@ public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
 
         mapa = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        mapa.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("La marca se encuentra en Sidney"));
-        mapa.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mapa.setMyLocationEnabled(true);
+
+        mapa.getUiSettings().setMyLocationButtonEnabled(false);
+
+        LocationManager locationManager = (LocationManager) Mapa.this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng miUbicacion = new LatLng(location.getLatitude(), location.getLongitude());
+                mapa.addMarker(new MarkerOptions().position(miUbicacion).title("ubicacion actual"));
+                mapa.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion));
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(miUbicacion)
+                        .zoom(14)
+                        .bearing(90)
+                        .tilt(45)
+                        .build();
+                mapa.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                LatLng medac = new LatLng(40.283828, -3.783878);
+                LatLng medac2 = new LatLng(40.283648, -3.784050);
+
+                mapa.addMarker(new MarkerOptions()
+                        .position(medac)
+                        .title("MEDAC Fuenlabrada"));
+                mapa.addMarker(new MarkerOptions()
+                        .position(medac2));
+                mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(medac, 15));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        /*        LatLng "id_plan" = new LatLng("Latitud", "Longitud");
+        *  mapa.addMarker(new MarkerOptions()
+                .position("id_plan")
+                .title("Titulo"));
+        * */
+
+        };
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 }
