14225d9fc329838ab5f58b4df9b1a5359536bcc0
dpb0001 <dpb0001@alu.medac.es>
2022-06-07 17:59:37 +0200

    Se actualiza el gradle y el manifest

--- app/src/main/java/com/dominiopersonal/luxianapp/mapas/Mapa.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/mapas/Mapa.java
@@ -1,4 +1,4 @@
-package com.dominiopersonal.luxianapp;
+package com.dominiopersonal.luxianapp.mapas;
 
 import android.Manifest;
 import android.content.Context;
@@ -12,13 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.dominiopersonal.luxianapp.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
--- app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
@@ -6,18 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.dominiopersonal.luxianapp.AccesoApp;
-import com.dominiopersonal.luxianapp.Mapa;
+import com.dominiopersonal.luxianapp.mapas.Mapa;
 import com.dominiopersonal.luxianapp.R;
 import com.dominiopersonal.luxianapp.databinding.FragmentFavoritosBinding;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class FavoritosFragment extends Fragment {
 
