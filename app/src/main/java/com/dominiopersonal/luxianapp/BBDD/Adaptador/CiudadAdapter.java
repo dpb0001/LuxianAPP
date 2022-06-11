fd83786c7515a9c1148292977b34cc82feabd8d5
dpb0001 <dpb0001@alu.medac.es>
2022-06-11 23:28:57 +0200

    Ya se coge toda la información de la BBDD y se visualiza correctamente

--- app/src/main/java/com/dominiopersonal/luxianapp/BBDD/Adaptador/CiudadAdapter.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/BBDD/Adaptador/CiudadAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.dominiopersonal.luxianapp.BBDD.Modelo.Ciudad;
 import com.dominiopersonal.luxianapp.R;
 import com.dominiopersonal.luxianapp.ui.home.HomeFragment;
@@ -45,9 +47,21 @@ public class CiudadAdapter extends RecyclerView.Adapter<CiudadAdapter.MyViewHold
 
         Ciudad ciudad = ciudadArrayList.get(position);
 
+        Glide.with(context)
+                .asBitmap()
+                .load(ciudad.getFoto())
+                .into(holder.imagen_ciudad);
+
         holder.nombre_ciudad.setText(ciudad.getNombre());
         holder.descripcion.setText(ciudad.getDescripción());
-        holder.imagen_ciudad.setImageURI(ciudad.getFoto());
+        holder.imagen_ciudad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
 
     }
@@ -58,7 +72,7 @@ public class CiudadAdapter extends RecyclerView.Adapter<CiudadAdapter.MyViewHold
     }
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView imagen_ciudad;
+        ImageView imagen_ciudad;
         TextView nombre_ciudad, descripcion;
 
         public MyViewHolder(@NonNull View itemView) {
