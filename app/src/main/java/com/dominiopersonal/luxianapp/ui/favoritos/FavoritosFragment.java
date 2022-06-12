e68c7fdea8627c027168d303b1b61aa7189b2b7b
NombreTemporalParaClase <dsi0001@alu.medac.es>
2022-06-12 15:29:19 +0200

    Cambiando el layout de favoritos

--- app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
+++ app/src/main/java/com/dominiopersonal/luxianapp/ui/favoritos/FavoritosFragment.java
@@ -1,6 +1,5 @@
 package com.dominiopersonal.luxianapp.ui.favoritos;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.dominiopersonal.luxianapp.mapas.Mapa;
-import com.dominiopersonal.luxianapp.R;
 import com.dominiopersonal.luxianapp.databinding.FragmentFavoritosBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
 
 public class FavoritosFragment extends Fragment {
 
@@ -53,22 +49,6 @@ public class FavoritosFragment extends Fragment {
         });
 
         // Final Obtener Datos
-
-        btnPruebasMapas = root.findViewById(R.id.btnPruebaMapas);
-
-
-        btnPruebasMapas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getContext(), Mapa.class);
-                i.putExtra("Latitud", latitud);
-                i.putExtra("Longitud", longitud);
-
-                startActivity(i);
-
-            }
-
-        });
 
         return root;
     }
